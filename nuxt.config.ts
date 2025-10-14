4318198a3b9581daaed3e06427746db1fb6339cd
boddemk <boddemk@amazon.com>
2025-10-14 23:31:18 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   devtools: { enabled: true }
+// })
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  nitro: {
+    awsAmplify: {
+        runtime: "nodejs18.x",
+    }
 })
